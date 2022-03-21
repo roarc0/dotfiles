e4ba134d81921f2f32fdfaefd8e15173c9615e33
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2022-03-21 14:52:43 +0100

    update gnome-setup.sh

--- scripts/setup-gnome.sh
+++ scripts/setup-gnome.sh
@@ -3,12 +3,15 @@
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 
 gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.desktop.session idle-delay 900
 
 gsettings set org.gnome.settings-daemon.plugins.media-keys max-screencast-length 3600
 
-gsettings org.gnome.nautilus.preferences click-policy 'single'
-gsettings org.gnome.nautilus.preferences always-use-location-entry true
-gsettings org.gnome.nautilus.preferences show-create-link true
-gsettings org.gnome.nautilus.compression default-compression-format 'tar.xz'
+gsettings set org.gnome.gnome-screenshot last-save-directory 'file:///home/roarc/pictures/screenshots'
+
+gsettings set org.gnome.nautilus.preferences click-policy 'single'
+gsettings set org.gnome.nautilus.preferences always-use-location-entry true
+gsettings set org.gnome.nautilus.preferences show-create-link true
+gsettings set org.gnome.nautilus.compression default-compression-format 'tar.xz'
 
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
