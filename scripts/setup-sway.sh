b558a84d3017584b37d59a7587537ac556f3f828
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2024-05-09 09:10:32 +0200

    update sway conf wip

--- scripts/setup-sway.sh
+++ scripts/setup-sway.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env sh
 
-FILE_PATH="/usr/share/xdg-desktop-portal/portals/gnome-keyring.portal"
+FILE_PATH="/usr/share/wayland-sessions/sway.desktop"
 
-if [ ! -f "$FILE_PATH" ]; then
-    echo "File $FILE_PATH does not exist."
-    exit 1
-fi
+# Use sed to perform the replacement
+sudo sed -i 's/^Exec=sway$/Exec=env XDG_CURRENT_DESKTOP=sway dbus-run-session sway/g' "$FILE_PATH"
 
-if grep -q ";sway" "$FILE_PATH"; then
-    echo "';sway' already present in $FILE_PATH"
-else
-    sed -i '/^UseIn=/ s/$/;sway/' "$FILE_PATH"
-    echo "Added ';sway' to $FILE_PATH"
-fi
+
+#FILE_PATH="/usr/share/xdg-desktop-portal/portals/gnome-keyring.portal"
+
+#if [ ! -f "$FILE_PATH" ]; then
+#    echo "File $FILE_PATH does not exist."
+#    exit 1
+#fi
+
+#if grep -q ";sway" "$FILE_PATH"; then
+#    echo "';sway' already present in $FILE_PATH"
+#else
+#    sed -i '/^UseIn=/ s/$/;sway/' "$FILE_PATH"
+#    echo "Added ';sway' to $FILE_PATH"
+#fi
