28cb0b0c407b7fc58521af7db5ca0d8a8f85525a
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2024-05-27 18:51:14 +0200

    add powerlevel stuff

--- scripts/setup-oh-my-zsh.sh
+++ scripts/setup-oh-my-zsh.sh
@@ -3,7 +3,9 @@
 OH_MY_ZSH="$HOME/.oh-my-zsh/"
 OH_MY_ZSH_PLUGINS="$OH_MY_ZSH/plugins/"
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d "$OH_MY_ZSH" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 if [ ! -d "$OH_MY_ZSH_PLUGINS"/zsh-autosuggestions ]; then
   git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions "$OH_MY_ZSH_PLUGINS"/zsh-autosuggestions || true
@@ -16,4 +18,8 @@ fi
 
 if [ ! -d "$OH_MY_ZSH_PLUGINS"/zsh-syntax-highlighting ]; then
   git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting "$OH_MY_ZSH_PLUGINS"/zsh-syntax-highlighting || true
+fi
+
+if [ ! -d "$OH_MY_ZSH"/themes/powerlevel10k ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
