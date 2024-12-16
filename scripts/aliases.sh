244805b23982facbcbbd5c89180cf0a05e03bec0
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2024-12-16 12:41:50 +0100

    update

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -23,6 +23,8 @@ alias df='df -kTh'
 alias mtop="ps --no-header -eo pmem,size,vsize,comm | sort -nr | sed 10q"
 alias ctop="ps --no-header -eo pcpu,comm | sort -nr | sed 10q"
 alias memrss='while read command percent rss; do if [[ "${command}" != "COMMAND" ]]; then rss="$(bc <<< "scale=2;${rss}/1024")"; fi; printf "%-26s%-8s%s\n" "${command}" "${percent}" "${rss}"; done < <(ps -A --sort -rss -o comm,pmem,rss | head -n 11)'
+alias millis='date +%s%3N'
+alias gitignore-sort='sort -u .gitignore -o .gitignore'
 
 #system
 alias reboot='sudo systemctl reboot'
